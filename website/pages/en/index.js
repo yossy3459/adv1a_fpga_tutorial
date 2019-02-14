bf55a936d01eefd647e8c9f31801c793f690ff17
yossy3459 <yoshi3459@outlook.com>
2019-02-15 04:42:31 +0900

    initial commit

---
+++ website/pages/en/index.js
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle">
+        {siteConfig.title}
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <ProjectTitle siteConfig={siteConfig} />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const BlockLeft = props => (
+      <Container
+        padding={['left', 'right']}
+        id={props.id}
+        background="light">
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const BlockCenter = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const Description = () => (
+      <BlockLeft id="index_desc">
+        {[
+          {
+            content: 'CADツール(Vivado)を用いたFPGAの設計方法を理解するための補助教材です。\
+                      この教材を参考に、実際に手を動かしながら、LSIの設計方法を学習していきましょう。\
+                      わからないところがあったら質問するようにしましょう。',
+            image: `${baseUrl}img/basys3.png`,
+            imageAlign: 'left',
+            title: 'FPGAを用いたLSIチップの設計演習用資料',
+          },
+        ]}
+      </BlockLeft>
+    );
+
+
+    const Intro = () => (
+      <div
+        className="paddingBottom"
+        style={{textAlign: 'center'}}>
+        <h2>この教材の使い方</h2>
+        <p class="index_intro">
+          「Basic」を参照し、授業課題を進めましょう。<br></br>
+           基本的に「Basic」を参照すれば、課題に取り組むことができます。<br></br>
+          「Advanced」では、よりツールを簡単に使う方法や、より踏み込んだ内容などを解説しています。<br></br>
+          「Help」では、リファレンスなどを紹介しています。<br></br>
+        </p>
+      </div>
+    );
+
+
+    const Features = () => (
+      <BlockCenter layout="threeColumn">
+        {[
+          {
+            content: 'まずはここから',
+            image: `${baseUrl}img/docusaurus.svg`,
+            imageAlign: 'top',
+            imageLink: 'docs/doc1.md'
+          },
+          {
+            content: 'より効率よく作業したいとき',
+            image: `${baseUrl}img/docusaurus.svg`,
+            imageAlign: 'top',
+            imageLink: 'docs/doc1.md'
+          },
+          {
+            content: 'マニュアルなど',
+            image: `${baseUrl}img/docusaurus.svg`,
+            imageAlign: 'top',
+            imageLink: 'docs/doc1.md'
+          },
+        ]}
+      </BlockCenter>
+    );
+
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+
+          <Description />
+          <Intro />
+          <Features />
+
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
---
+++ website/siteConfig.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'Kimiyoshi Usami',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+    image: '/img/usamilab.png',
+    infoLink: 'http://www.usamilab.ise.shibaura-it.ac.jp/',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: '高度情報演習2A FPGA開発チュートリアル', // Title for your website.
+  tagline: '担当: 宇佐美 公良',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'koudo2a_fpga_tutorial',
+  organizationName: 'UsamiLab.',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Basic'},
+    {doc: 'doc2', label: 'Advanced'},
+    {page: 'help', label: 'Help'},
+    {href: "http://syllabus.sic.shibaura-it.ac.jp/syllabus/2018/ko1/105270.html?y=2018&g=L01", label: "Syllabus" },
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/usamilab.png',
+  footerIcon: 'img/usamilab.png',
+  favicon: 'img/usamilab.png',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#3F51B5',
+    secondaryColor: '#7986CB',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} UsamiLab`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  // ogImage: 'img/docusaurus.png',
+  // twitterImage: 'img/docusaurus.png',
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
