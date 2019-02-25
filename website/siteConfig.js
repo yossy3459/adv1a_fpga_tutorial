fcd849c8f50f65d411317f7f6c324e3352602222
yossy3459 <yoshi3459@outlook.com>
2019-02-26 00:55:58 +0700

    projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'koudo2a_fpga_tutorial',
+  projectName: 'koudo2a_fpga_tutorial_wip',
   organizationName: 'yossy3459',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
