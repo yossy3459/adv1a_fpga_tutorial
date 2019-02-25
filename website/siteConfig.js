2203073c0764da6f821f5b329ee117fffd0c60d7
yossy3459 <yoshi3459@outlook.com>
2019-02-26 00:51:48 +0700

    organizationName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'koudo2a_fpga_tutorial',
-  organizationName: 'UsamiLab.',
+  organizationName: 'yossy3459',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
